33687827e2a4f513167ac51441aace15d86123f3
sans-dev-self <sans.developer.main@gmail.com>
2026-04-15 16:10:53 +0300

    smile

--- task.py
+++ task.py
@@ -1,13 +1,1 @@
-num = int(input("Введите число: "))
-n = int(input("В какую систему: "))
-
-result = ""
-while num > 0:
-    rem = num % n
-    if 10 <= rem <= 16:
-        result += chr(55 + rem)
-    else:
-        result += str(rem)
-    num //= n
-print(result[::-1])
-#
+print("🤗🙂😎🤔🤣")
